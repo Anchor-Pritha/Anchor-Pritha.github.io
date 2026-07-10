539653d33542f976222bc370c075b9880e594963
Anchorpritha <prithabahel04@gmail.com>
2026-07-10 20:39:57 +0530

    Change whole website

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Hello this is my repository which hosts a website
+# Hello this is Anchor pritha do sure to check my website by following the link
+
 You can follow the link to my website
 https://anchor-pritha.github.io
+Thank you
--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//Blank for now will be used for future scripts
+//Variables
+const elements = document.querySelectorAll(".reveal", "reveal-left");
+const observer = new IntersectionObserver(
+  (entries) => {
+    // console.log(entries);
+    entries.forEach((entry) => {
+      // console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+elements.forEach((element) => {
+  observer.observe(element);
+});
