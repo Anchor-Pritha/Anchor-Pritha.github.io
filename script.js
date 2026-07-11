612f60bfd8ec4d353ba61b4573fe94f036f252c2
Anchorpritha <prithabahel04@gmail.com>
2026-07-11 23:18:37 +0530

    update in ui made website more responsive

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //Variables
 const elements = document.querySelectorAll(".reveal", "reveal-left");
 const image = document.querySelector("#image");
-const observer = new IntersectionObserver(
+// Variable declared with let to allow reassignment of the observer variable
+let observer = new IntersectionObserver(
   (entries) => {
     // console.log(entries);
     entries.forEach((entry) => {
@@ -22,8 +23,21 @@ elements.forEach((element) => {
 // Check if the width of the window object is less than 750px (for mobile devices) and remove the reveal class
 function calcWidth() {
   if (window.innerWidth <= 750) {
-    image.classList.remove(".reveal");
-    console.log(`calculating width`);
+    image.classList.remove("reveal");
+    observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("active");
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        // threshold set to 1 to ensure the entire element is visible before triggering the animation
+        threshold: 1,
+      },
+    );
   }
 }
 // Call when page load
