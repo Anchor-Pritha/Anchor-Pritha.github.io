4b7e0aa1c84d9e6b5b6dd4f14c1829d11129dc67
Anchorpritha <prithabahel04@gmail.com>
2026-07-11 16:39:44 +0530

    Change js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Variables
 const elements = document.querySelectorAll(".reveal", "reveal-left");
+const image = document.querySelector("#image");
 const observer = new IntersectionObserver(
   (entries) => {
     // console.log(entries);
@@ -18,3 +19,14 @@ const observer = new IntersectionObserver(
 elements.forEach((element) => {
   observer.observe(element);
 });
+// Check if the width of the window object is less than 750px (for mobile devices) and remove the reveal class
+function calcWidth() {
+  if (window.innerWidth <= 750) {
+    image.classList.remove(".reveal");
+    console.log(`calculating width`);
+  }
+}
+// Call when page load
+calcWidth();
+// Call when user resizes
+window.addEventListener("resize", calcWidth);
